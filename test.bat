33721781f0828f4f06f8aee44128e4cbde11a3af
Mateusz Karabela <m.karabela@PREVAC.EU.local>
2017-01-18 09:31:11 +0100

    Some non-important clean-up

--- test.bat
+++ test.bat
@@ -1,4 +1,2 @@
-::python PCSDiff.py "Y:\\Projekty\\!Opisy aparatur\\511_Bangalore\\Schematy\\511_schem.PRO" "Y:\\Projekty\\!Opisy aparatur\\511_Bangalore\\Schematy\\511_schemt_test.PRO"
 
-::python PCSDiff.py "Y:\Projekty\!Opisy aparatur\506_ARPES_PV\Schematy\506_RACK1v4.PRO" "Y:\Projekty\!Opisy aparatur\506_ARPES_PV\Schematy\506_RACK1v4_.PRO"
 python test.py
